ba7efcdb2abf54188cc3bea2257f94c57fa3346d
carlos-dev <apolayamendozacarlos@gmail.com>
2024-07-01 18:12:46 -0500

    Se crea el header de la pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import MainHeader from "@/component/MainHeader"
 
 const page = () => {
   return (
-    <div>
+    <div className="container">
       <MainHeader />
     </div>
   )
--- src/component/MainHeader.tsx
+++ src/component/MainHeader.tsx
@@ -1,6 +1,6 @@
 const MainHeader = () => {
   return (
-    <div>
+    <div className='Main-Header'>
       <img src="/images/User.png" alt="User" className="user"/>
       <h1>
         Carlos David
