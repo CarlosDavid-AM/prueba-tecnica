bf90717a17a53d08babf1f4705e0aca641242b36
carlos-dev <apolayamendozacarlos@gmail.com>
2024-07-01 17:52:13 -0500

    First Commit

---
+++ src/component/MainHeader.tsx
@@ -0,0 +1,12 @@
+const MainHeader = () => {
+  return (
+    <div>
+      <img src="/images/User.png" alt="User" className="user"/>
+      <h1>
+        Carlos David
+      </h1>
+    </div>
+  )
+}
+
+export default MainHeader
